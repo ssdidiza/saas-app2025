64201f441b143167fb272f4cfd8deaeb4131c5e1
didiserv <siphethuxolo@didiserv.co.za>
2025-05-25 15:01:26 +0200

    implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: 'Home', href: '/' },
+  { label: 'Companions', href: '/companions' },
+  { label: 'My Journey', href: '/my-journey' },
+];
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(
+            "text-gray-700 hover:text-gray-900",
+            pathname === href && "text-primary font-semibold"
+          )}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
